d1fbfdd5f4f78ea370d419fb4d8de16482390220
Tatiana Romanova <tanya_rom@inbox.ru>
2021-12-12 20:08:25 +0300

    added solutions for day1 tasks

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int count = 0;
+        while (count < 10) {
+            System.out.println("JAVA");
+            count++;
+        }
     }
 }
--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+        int k = 3;
+        int i = 1;
 
+        while (i <= 9) {
+            System.out.println(i + " x " + k + " = " + i * k);
+            i++;
+        }
     }
 }
