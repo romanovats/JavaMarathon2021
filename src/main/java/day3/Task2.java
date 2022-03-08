154493f354b736af5d0b105358e541d8fdd47cf0
Tatiana Romanova <tanya_rom@inbox.ru>
2022-03-08 21:30:34 +0300

    fix task2 from day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -6,15 +6,14 @@ public class Task2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        double a = scanner.nextDouble();
-        double b = scanner.nextDouble();
 
         while (true) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
             if (b == 0) {
                 break;
             } else {
                 System.out.println(a / b);
-                break;
             }
         }
     }
