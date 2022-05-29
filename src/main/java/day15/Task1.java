2661ecd0deabeb3699d0d97bdc6213c5a3dd7ffe
Tatiana Romanova <tanya_rom@inbox.ru>
2022-05-29 23:35:19 +0300

    added day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,26 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("src/main/resources/shoes.csv");
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            String[] listShoes = (scanner.nextLine()).split(";");
+
+            if(Integer.parseInt(listShoes[2]) == 0) {
+                System.out.print(listShoes[0] + ", " + listShoes[1] + "," + listShoes[2] +"\n");
+            }
+        }
+
+
 
     }
 }
